46b03bbcca63c76badd234d3a91f80ca671cd1e6
indexofmayank <maimayanmanu@gmail.com>
2022-09-20 10:26:22 +0530

    today

---
+++ champbackup/index.php
@@ -0,0 +1,31 @@
+<?php
+
+//print_r($_SERVER);die();
+
+$sitepad['db_name'] = 'gigaaii1_spad259';
+$sitepad['db_user'] = 'gigaaii1_spad259';
+$sitepad['db_pass'] = 'cSQ5[X!y.b';
+$sitepad['db_host'] = 'localhost';
+$sitepad['db_table_prefix'] = 'zYiWJ9b_';
+$sitepad['charset'] = 'utf8mb4';
+$sitepad['collate'] = 'utf8mb4_unicode_ci';
+$sitepad['serving_url'] = 'sankalpshristi.org/champbackup';// URL without protocol but with directory as well
+$sitepad['url'] = 'https://sankalpshristi.org/champbackup';
+$sitepad['relativeurl'] = '/champbackup';
+$sitepad['.sitepad'] = '/home/gigaaii1';
+$sitepad['sitepad_plugin_path'] = '/usr/local/sitepad';
+$sitepad['editor_path'] = '/usr/local/sitepad/editor';
+$sitepad['path'] = dirname(__FILE__);
+$sitepad['AUTH_KEY'] = 'L94Lw8l3yQVtO3HMwvmvveE0yPKQLS5CTBFAXedwzVaWhaffrmcmdbrqHDJqtV5C';
+$sitepad['SECURE_AUTH_KEY'] = 'RgzAJn9VUgpdGQTgxeBjCRSvYcZQ3e9UpPPTMfZ4UCIQs24pXbPKgSWEYnGgXRI9';
+$sitepad['LOGGED_IN_KEY'] = '9Ybk4wuGi5pCXZrDhHEhXOtwoipe7qrCLLQL5oVW1WKneO64DCa8oaCF7sFdssfd';
+$sitepad['NONCE_KEY'] = 'tx7NNDo8t2xf8PXHkm3xPOiFoVC6oNsK6dC9GubYZr2D3M2bgQsO1Vi38mF5ff8a';
+$sitepad['AUTH_SALT'] = 'CEFgpEV9LWVinjXwBkNW274ZYEMrInMf43mbRAIGjEe61tEOniyfuQrQ3qOeZVLy';
+$sitepad['SECURE_AUTH_SALT'] = 'HauKfqQhrcpQ7zReOyTZk1pqQf5gGIONDBXc2u9dlVVIWV9G5VtrNLbbtsKbhq3a';
+$sitepad['LOGGED_IN_SALT'] = 'uzFz00pnV6LonPP1x7jAazfFeZsesaG6UO0d96ncOuVyObCuiwIkytNrmicJur8N';
+$sitepad['NONCE_SALT'] = '47bxtarETIaoAjFUYhNrUjHSCZXWmq4Mq5pVE65hifo6a8cgwqtk3LB9PfVXfMlP';
+
+if(!include_once($sitepad['editor_path'].'/site-inc/bootstrap.php')){
+	die('Could not include the bootstrap.php. One of the reasons could be open_basedir restriction !');
+}
+
